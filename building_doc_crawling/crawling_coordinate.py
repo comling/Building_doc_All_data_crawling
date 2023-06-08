99a41470ac32ffc8ceca586ae7b8110630c3e922
Wonchul <comling2497@gmail.com>
2023-06-08 18:16:53 +0900

    좌표확보 자동 저장 및 requirements 생성

--- building_doc_crawling/crawling_coordinate.py
+++ building_doc_crawling/crawling_coordinate.py
@@ -36,92 +36,107 @@ def geocode_parse(df, process_cnt):
     errorCode = soup.find('errorCode').get_text()
     errorMessage = soup.find('errorMessage').get_text()
 
-    if totalCount != '0' and errorCode == '0':
-        bdMgtSn = soup.find('bdMgtSn').get_text()  # 건물관리번호
-        entX = soup.find('entX').get_text()  # X좌표(UTM-K/GRS80[EPSG:5179])
-        entY = soup.find('entY').get_text()  # Y좌표(UTM-K/GRS80[EPSG:5179])
-        bdNm = soup.find('bdNm').get_text()  # 건물명
+    try:
+        if totalCount != '0' and errorCode == '0':
+            bdMgtSn = soup.find('bdMgtSn').get_text()  # 건물관리번호
+            entX = soup.find('entX').get_text()  # X좌표(UTM-K/GRS80[EPSG:5179])
+            entY = soup.find('entY').get_text()  # Y좌표(UTM-K/GRS80[EPSG:5179])
+            bdNm = soup.find('bdNm').get_text()  # 건물명
 
-        # 좌표계 정의
-        entUTMK = Proj(init='epsg:5179')  # UTM-K(GRS80) 주소별 좌표
-        entWGS84 = Proj(init='epsg:4326')  # WGS84 경도/위도, GPS 사용 전지구 좌표
+            # 좌표계 정의
+            entUTMK = Proj(init='epsg:5179')  # UTM-K(GRS80) 주소별 좌표
+            entWGS84 = Proj(init='epsg:4326')  # WGS84 경도/위도, GPS 사용 전지구 좌표
 
-        WGS84_X, WGS84_Y = transform(entUTMK, entWGS84, entX, entY)
+            WGS84_X, WGS84_Y = transform(entUTMK, entWGS84, entX, entY)
 
-    elif errorCode == '-999':
-        print(load_address, '-', errorMessage)
-    elif errorCode == 'E0001':
-        print(load_address, '-', errorMessage)
-    elif errorCode == 'E0002':
-        print(load_address, '-', errorMessage)
-    elif errorCode == 'E0003':
-        print(load_address, '-', errorMessage)
-    elif errorCode == 'E0004':
-        print(load_address, '-', errorMessage)
-    elif errorCode == 'E0005':
-        print(load_address, '-', errorMessage)
-    elif errorCode == 'E0006':
-        print(load_address, '-', errorMessage)
-    elif errorCode == 'E0007':
-        print(load_address, '-', errorMessage)
-    else:
-        print(load_address, '-', errorCode, ": ", errorMessage)
+        elif errorCode == '-999':
+            print(management_number, '-', errorMessage)
+        elif errorCode == 'E0001':
+            print(management_number, '-', errorMessage)
+        elif errorCode == 'E0002':
+            print(management_number, '-', errorMessage)
+        elif errorCode == 'E0003':
+            print(management_number, '-', errorMessage)
+        elif errorCode == 'E0004':
+            print(management_number, '-', errorMessage)
+        elif errorCode == 'E0005':
+            print(management_number, '-', errorMessage)
+        elif errorCode == 'E0006':
+            print(management_number, '-', errorMessage)
+        elif errorCode == 'E0007':
+            print(management_number, '-', errorMessage)
+        else:
+            print(management_number, '-', errorCode, ": ", errorMessage)
 
-    return {
-        'management_number': management_number,
-        'sido_name': sido_name,
-        'full_load_address': load_address,
-        'bdMgtSn': bdMgtSn,
-        'bdNm': bdNm,
-        'entX': entX,
-        'entY': entY,
-        'longitude': WGS84_X,
-        'latitude': WGS84_Y
-    }
+        coordinate.append({
+            'management_number': management_number,
+            'sido_name': sido_name,
+            'full_load_address': load_address,
+            'bdMgtSn': bdMgtSn,
+            'bdNm': bdNm,
+            'entX': entX,
+            'entY': entY,
+            'longitude': WGS84_X,
+            'latitude': WGS84_Y
+        })
+    except Exception as e:
+        coordinate_error.append({
+            'management_number': df['management_number'][process_cnt],
+            'sido_name': df['sido_name'][process_cnt],
+            'full_load_address': df['full_load_address'][process_cnt],
+            'admCd': df['admCd'][process_cnt],
+            'rnMgtSn': df['rnMgtSn'][process_cnt],
+            'udrtYn': df['udrtYn'][process_cnt],
+            'buldMnnm': df['buldMnnm'][process_cnt],
+            'buldSlno': df['buldSlno'][process_cnt]
+        })
+        pass
 
 if __name__ == '__main__':
 
     engine = create_engine(f'mariadb+pymysql://{config.user}:{config.pwd}@{config.host}:3306/{config.db}', echo=False)
-    conn = engine.connect()
+
     while True:
-        try:
-            sql = text('''
-                    select  distinct management_number, 
-                            sido_name,
-                            apartment_flag, 
-                            delivery_name, 
-                            document_build_name, 
-                            sigungu_build_name, 
-                            full_load_address, 
-                            beopdong_code as admCd, 
-                            load_code as rnMgtSn, 
-                            underground_flag as udrtYn, 
-                            build_1st_number as buldMnnm, 
-                            build_2nd_number as buldSlno 
-                    from zz_all
-                    where management_number not in (select management_number from zz_coordinate) 
-                    
-                    limit 10000
-                    ''')
+        conn = engine.connect()
+        sql_coordinate = text('''
+                select  distinct management_number, 
+                        sido_name,
+                        apartment_flag, 
+                        delivery_name, 
+                        document_build_name, 
+                        sigungu_build_name, 
+                        full_load_address, 
+                        beopdong_code as admCd, 
+                        load_code as rnMgtSn, 
+                        underground_flag as udrtYn, 
+                        build_1st_number as buldMnnm, 
+                        build_2nd_number as buldSlno 
+                from zz_all
+                where management_number not in (select management_number from zz_coordinate)
+                  and management_number not in (select management_number from zz_coordinate_error)
+                limit 100
+                ''')
 
-            df = pd.read_sql_query(sql, conn)
-            coordinate = []  # 좌표
+        sql_coordinate_df = pd.read_sql_query(sql_coordinate, conn)
+        time.sleep(1)
+        global coordinate, coordinate_error
+        coordinate = []  # 좌표
+        coordinate_error = []
 
-            for process_cnt in tqdm(range(0, len(df['full_load_address'])), total=len(df['full_load_address']), desc='좌표획득 진행율', ncols=100, ascii=' =', leave=True):
-                coordinate.append(geocode_parse(df, process_cnt))
+        for process_cnt in tqdm(range(0, len(sql_coordinate_df['full_load_address'])), total=len(sql_coordinate_df['full_load_address']), desc='좌표획득 진행율', ncols=100, ascii=' =', leave=True):
+            geocode_parse(sql_coordinate_df, process_cnt)
+            time.sleep(0.5)
 
-            coordinate_df = pd.DataFrame(coordinate)
+        coordinate_df = pd.DataFrame(coordinate)
+        coordinate_error_df = pd.DataFrame(coordinate_error)
+        # Bulk Data DB 업로드 속도 개선 기능
+        @event.listens_for(engine, "before_cursor_execute")
+        def receive_before_cursor_execute(
+                conn, cursor, statement, params, context, executemany
+        ):
+            if executemany:
+                cursor.fast_executemany = True
 
-            # Bulk Data DB 업로드 속도 개선 기능
-            @event.listens_for(engine, "before_cursor_execute")
-            def receive_before_cursor_execute(
-                    conn, cursor, statement, params, context, executemany
-            ):
-                if executemany:
-                    cursor.fast_executemany = True
+        coordinate_df.to_sql('zz_coordinate', engine, index=False, if_exists="append", chunksize=100000)
+        coordinate_error_df.to_sql('zz_coordinate_error', engine, index=False, if_exists="append", chunksize=100000)
 
-            coordinate_df.to_sql('zz_coordinate', engine, index=False, if_exists="append", chunksize=100000)
-
-        except Exception as e:
-            print('좌표획득 프로세스 오류: ', e)
-            pass
